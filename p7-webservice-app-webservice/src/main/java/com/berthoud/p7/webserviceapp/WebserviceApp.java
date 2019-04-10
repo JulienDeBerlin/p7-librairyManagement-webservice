edefd924e3b01df8ec580f54b48c1ef7c29821a2
JulienDeBerlin <julien_berthoud@yahoo.fr>
2019-04-10 16:02:41 +0200

    Project-squeleton: Springboot starts

---
+++ p7-webservice-app-webservice/src/main/java/com/berthoud/p7/webserviceapp/WebserviceApp.java
@@ -0,0 +1,13 @@
+package com.berthoud.p7.webserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebserviceApp.class, args);
+    }
+
+}
