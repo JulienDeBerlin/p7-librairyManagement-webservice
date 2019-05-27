dd798fe36f2259a8571228d1cb322bf6f3c53aa2
JulienDeBerlin <julien_berthoud@yahoo.fr>
2019-05-27 12:03:19 +0200

    code cleaning

--- p7-webservice-app-webservice/src/test/java/com/berthoud/p7/webserviceapp/tests/Tests_LoanManagement.java
+++ p7-webservice-app-webservice/src/test/java/com/berthoud/p7/webserviceapp/tests/Tests_LoanManagement.java
@@ -110,16 +110,16 @@ public class Tests_LoanManagement {
     public void testMonitoringLoans() {
 
         List<Loan> listLoansLate = loanManager.getOpenLoansLate();
-        assertEquals(listLoansLate.size(), 5);
+        assertEquals(listLoansLate.size(), 4);
 
         List<Loan> listLoansInTime = loanManager.getOpenLoansInTime();
-        assertEquals(listLoansInTime.size(), 2);
+        assertEquals(listLoansInTime.size(), 3);
 
         List<Loan> listAllLoans = loanManager.getAllOpenLoans();
         assertEquals(listAllLoans.size(), 7);
 
         List<Loan> listOpenLoansExtended = loanManager.getOpenLoansExtended();
-        assertEquals(listOpenLoansExtended.size(), 5);
+        assertEquals(listOpenLoansExtended.size(), 6);
 
     }
 
